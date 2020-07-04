44c9bee79cd78c69547adc50586be3f9b28a25ca
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-07-04 15:03:54 -0300

    Migration completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 end
